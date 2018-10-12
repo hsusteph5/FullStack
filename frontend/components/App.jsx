99ac9cbfc62c2c6a603a7fe53c6dfc0f9c217b34
Stephanie Hsu <hsusteph5@gmail.com>
2018-10-11 17:57:56 -0700

    Finish Auth Routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import LoginFormContainer from './user_auth_form/login_form_container';
 import SignupFormContainer from './user_auth_form/signup_form_container';
@@ -8,15 +9,9 @@ import Splash from './splash/splash';
 const App = () => (
   <div>
     <Route exact path='/' component={Splash} />
-    <Route exact path='/login' component={LoginFormContainer} />
-    <Route exact path='/signup' component={SignupFormContainer} />
+    <AuthRoute exact path='/login' component={LoginFormContainer} />
+    <AuthRoute exact path='/signup' component={SignupFormContainer} />
   </div>
 );
 
 export default App;
-
-
-// <header>
-//   <h1>Hello, this is Yip</h1>
-//   <NavBarContainer />
-// </header>
