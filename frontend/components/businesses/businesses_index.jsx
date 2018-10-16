5b08d7bbf8245ac25f624c40d26747cd76ee7a4c
Stephanie Hsu <hsusteph5@gmail.com>
2018-10-16 12:12:43 -0700

    Add categories CSS with a write review button

--- frontend/components/businesses/businesses_index.jsx
+++ frontend/components/businesses/businesses_index.jsx
@@ -33,7 +33,7 @@ class BusinessesIndex extends React.Component {
 
       //decode URI parses out of the white spaces %20
       let decodeParams = decodeURI(parseParams);
-      //save the query string into the local state and save the category into the local state 
+      //save the query string into the local state and save the category into the local state
       this.setState({query: searchParams, name: decodeParams});
 
       //since you are not visiting a new route, you need to see if the current params looks
@@ -68,6 +68,10 @@ class BusinessesIndex extends React.Component {
 
         <div className="background-categories-container">
           <CategoriesContainer />
+          <button className="categories-write-review">
+            <i className="fas fa-pencil-alt"></i>
+            Write a Review
+          </button>
         </div>
 
         <div className="best-in-SF-background">
