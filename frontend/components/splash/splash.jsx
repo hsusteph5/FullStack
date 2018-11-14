2031878f960a8099ff176bb9256913a8709a4664
Stephanie Hsu <hsusteph5@gmail.com>
2018-11-14 12:37:25 -0800

    Add footer portions About, About business

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -34,13 +34,43 @@ class Splash extends React.Component {
         <footer className="credits">
           <div className="all-credits">
             <div className="all-photo-credits">
-              <h3> Photo Credits </h3>
+              <h3>About</h3>
               <ul>
-                <li><p> Photo by rawpixel on Unsplash </p></li>
-                <li><p>Photo by Kaylah Otto on Unsplash</p></li>
-                <li><p>Photo by Oscar Nord on Unsplash</p></li>
-                <li><p>Photo by Pille-Riin Priske on Unsplash</p></li>
+                <li><p> About Yip </p></li>
+                <li><p>Careers</p></li>
+                <li><p>Press</p></li>
+                <li><p>Investor Relations</p></li>
+                <li><p>Content Guidelines</p></li>
+                <li><p>Terms of Service</p></li>
+                <li><p>Privacy Policy</p></li>
+                <li><p>Ad Choice</p></li>
               </ul>
+            </div>
+
+            <div>
+              <h3>Discover</h3>
+                <ul>
+                  <li><p>The Local Yip</p></li>
+                  <li><p>Yip Blog</p></li>
+                  <li><p>Support</p></li>
+                  <li><p>Yip Mobile</p></li>
+                  <li><p>Developers</p></li>
+                  <li><p>RSS</p></li>
+                </ul>
+            </div>
+
+            <div>
+              <h3>Yip for Business Owners</h3>
+                <ul>
+                  <li><p>Claim your Business Page</p></li>
+                  <li><p>Advertise for Yip</p></li>
+                  <li><p>Yip Reservations</p></li>
+                  <li><p>Yip Wifi</p></li>
+                  <li><p>Yip Nowait</p></li>
+                  <li><p>Business Sucess Stories</p></li>
+                  <li><p>Business Support</p></li>
+                  <li><p>Yip Blog for Business Owners</p></li>
+                </ul>
             </div>
 
             <div className="about-stephanie">
