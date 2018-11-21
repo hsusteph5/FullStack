700e44519e33bc89f8e7c3be82e896572383e166
Stephanie Hsu <hsusteph5@gmail.com>
2018-11-21 11:45:32 -0800

    Remove logger and fix click handler for browse category

--- frontend/components/splash/Yip_SF_Component.jsx
+++ frontend/components/splash/Yip_SF_Component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRatingStars } from '../../util/helper_functions/parsing_manager';
+import { Link } from 'react-router-dom';
 
 const YipSFComponent = ( { businesses } ) => {
     let businessHTML = (businesses.length !== 0) ? (
@@ -9,7 +10,7 @@ const YipSFComponent = ( { businesses } ) => {
                 <div className="splash-business-index" key={business.id}>
                     <img src={business.photoUrls[0]} style={{width: 225, height: 225, objectFit: 'cover'}}></img>
                     <div className="splash-business-description">
-                        <h4> {business.name} </h4>
+                        <h4> <Link to={{pathname: `/businesses/${business.id}`}}> {business.name} </Link> </h4>
                         <h5>{ stars }</h5>
                         <h5 id="splash-business-reviews">{business.num_reviews} reviews</h5>
                         <h5>San Francisco</h5>
--- frontend/components/splash/browse_category.jsx
+++ frontend/components/splash/browse_category.jsx
@@ -21,11 +21,11 @@ class BrowseCategory extends React.Component {
     //must fetch the business {name: category}
       searchBusinesses(category){
         return (e) => {
-          e.preventDefault();
-          this.setState({name: category});
-          this.props.fetchBusinesses(this.state)
-          .then(() => this.props.history.push({pathname:'/businesses', search: `?name=${this.state.name}`}))
-          .then(() => window.scrollTo(0,0));
+            e.preventDefault();
+            this.setState({name: category});
+            this.props.fetchBusinesses(this.state)
+                .then(() => this.props.history.push({pathname:'/businesses', search: `?name=${this.state.name}`}))
+                .then(() => window.scrollTo(0,0));
         }
       }
     
@@ -35,23 +35,23 @@ class BrowseCategory extends React.Component {
                 <h2>Browse Businesses by Category</h2>
                 <div className="browse-category-container">
                     
-                    <div className="splash-category-index">
-                        <img src= { coffee } className="splash-category-images" onClick={this.searchBusinesses('coffee & tea')}></img>
+                    <div className="splash-category-index" onClick={this.searchBusinesses('coffee & tea')}>
+                        <img src= { coffee } className="splash-category-images"></img>
                         <h4>Coffee & Tea</h4>
                     </div>
 
-                    <div className="splash-category-index">
-                        <img src = { iceCream } className="splash-category-images" onClick={this.searchBusinesses('dessert')}></img>
+                    <div className="splash-category-index" onClick={this.searchBusinesses('dessert')}>
+                        <img src = { iceCream } className="splash-category-images"></img>
                         <h4>Desserts</h4>
                     </div>
 
-                    <div className="splash-category-index">
-                        <img src = { restaurant } className="splash-category-images" onClick={this.searchBusinesses('cafe')}></img>
+                    <div className="splash-category-index" onClick={this.searchBusinesses('cafe')}>
+                        <img src = { restaurant } className="splash-category-images"></img>
                         <h4>Cafe</h4>
                     </div>
 
-                    <div className="splash-category-index">
-                        <img src = { tea } className="splash-category-images" onClick={this.searchBusinesses('bubble tea')}></img>
+                    <div className="splash-category-index" onClick={this.searchBusinesses('bubble tea')}>
+                        <img src = { tea } className="splash-category-images"></img>
                         <h4>Bubble tea</h4>
                     </div>
                 </div>
@@ -61,72 +61,4 @@ class BrowseCategory extends React.Component {
     
 }
 
-/* <div className="background-search-icons">
-            <div className="search-icons-container" onClick={this.searchBusinesses('coffee & tea')}>
-              <i className="fas fa-coffee"></i>
-              <button className="search-icon-button">Coffee & Tea</button>
-            </div>
-            <div className="search-icons-container" onClick={this.searchBusinesses('cafe')}>
-              <i className="fas fa-store"></i>
-              <button className="search-icon-button">Cafe</button>
-            </div>
-    
-            <div className="search-icons-container" onClick={this.searchBusinesses('dessert')}>
-              <i className="fas fa-birthday-cake"></i>
-              <button className="search-icon-button">Dessert</button>
-            </div>
-    
-</div> */
-
 export default withRouter(BrowseCategory);
-
-
-// constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       name: "",
-//     }
-//     this.searchBusinesses = this.searchBusinesses.bind(this);
-// }
-
-
-// searchBusinesses(category){
-//     return (e) => {
-//       e.preventDefault();
-//       this.setState({name: category});
-//       this.props.fetchBusinesses(this.state)
-//       .then(() => this.props.history.push({pathname:'/businesses', search: `?name=${this.state.name}`}));
-//     }
-// }
-
-// render() {
-//     return(
-//         <div className="browse-category"> 
-//             <h2>Browse Businesses by Category</h2>
-//             <div className="browse-category-container">
-                
-//                 <div className="splash-category-index">
-//                     <img src = { coffee } style={{width: 50, height: 50, objectFit: 'cover'}} onClick={this.searchBusinesses('coffee & tea')}></img>
-//                     <h4>Coffee & Tea</h4>
-//                 </div>
-
-//                 <div className="splash-category-index">
-//                     <img src = { iceCream } style={{width: 50, height: 50, objectFit: 'cover'}} onClick={this.searchBusinesses('dessert')}></img>
-//                     <h4>Desserts</h4>
-//                 </div>
-
-//                 <div className="splash-category-index">
-//                     <img src = { restaurant } style={{width: 50, height: 50, objectFit: 'cover'}} onClick={this.searchBusinesses('cafe')}></img>
-//                     <h4>Cafe</h4>
-//                 </div>
-
-//                 <div className="splash-category-index">
-//                     <img src = { tea } style={{width: 50, height: 50, objectFit: 'cover'}} onClick={this.searchBusinesses('bubble tea')}></img>
-//                     <h4>Bubble tea</h4>
-//                 </div>
-//             </div>
-
-//         </div>
-//     );
-// }
